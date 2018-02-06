df9b6c89ddfedbe5acdd533a4d2b932b8b7be0b4
Geno Roupsky <geno@roupsky.name>
2018-02-06 10:08:05 +0200

    Fix populata data extraction for special chambers

--- tools/bwriftpop.js
+++ tools/bwriftpop.js
@@ -2,6 +2,7 @@
 
 var jt = require("jacksmhtools-client");
 var utils = require("./_utils");
+var paladinBaneSuffix = ' (Paladin\'s Bane)'
 var stages = [
   "Acolyte",
   "Ancient Lab",
@@ -26,6 +27,16 @@ var cheeses = [
   "Swiss String",
   "Marble String"
 ]
+// Stages where Portal Pursuer is always available or Paladin's Bane have no effect
+var specialStages = [
+  "Entrance",
+  'Guard Barracks',
+  "Ingress",
+  "Pursuer Mousoleum",
+  "Security"
+]
+
+var normalStages = stages.filter(function (stage) { return specialStages.indexOf(stage) === -1})
 
 utils
   .process({
@@ -34,8 +45,8 @@ utils
       cheese: utils.genVarField("cheese", cheeses),
     },
     series: [
-      { // normal
-        phase: utils.genVarField("stage", stages),
+      { // normal chambers - no pursuer and no paladin's bane
+        phase: utils.genVarField("stage", normalStages),
         charm: [
           {
             vars: { charm: { "Rift Antiskele": false } },
@@ -47,9 +58,9 @@ utils
           }
         ]
       },
-      { // with Paladin's Bane buff
-        phase: utils.genVarField("stage", stages).map(function (options) {
-          options.fields.stage += ' (Paladin\'s Bane)'
+      { // normal chambers with Paladin's Bane buff
+        phase: utils.genVarField("stage", normalStages).map(function (options) {
+          options.fields.stage += paladinBaneSuffix
           return options
         }),
         charm: [
@@ -60,6 +71,75 @@ utils
           {
             fields: { charm: "Rift Antiskele" },
             opts: { exclude: [ 'Skeletal Champion', 'Portal Pursuer', 'Portal Paladin' ] }
+          }
+        ]
+      },
+      { // Entrance - have no buffs or curses
+        phase: utils.genVarField("stage", "Entrance"),
+        charm: [
+          {
+            vars: { charm: { "Rift Antiskele": false } },
+            opts: { exclude: [ 'Portal Pursuer' ] }
+          },
+          {
+            fields: { charm: "Rift Antiskele" },
+            opts: { exclude: [ 'Skeletal Champion', 'Portal Pursuer' ] }
+          }
+        ]
+      },
+      { // Guard Barracks - no paladin's bane and no skeletal champion
+        phase: utils.genVarField("stage", "Guard Barracks"),
+        noPursuer: [ { opts: { exclude: [ 'Portal Pursuer' ] } } ]
+      },
+      { // Ingress chamber - have Portal Pursuer but no Skeletal Champion
+        phase: [
+          {
+            vars: { stage: { "Ingress": true } },
+            fields: { stage: "Ingress" }
+          },
+          {
+            vars: { stage: { "Ingress": true } },
+            fields: { stage: "Ingress" + paladinBaneSuffix },
+            opts: { exclude: [ 'Portal Paladin' ] }
+          }
+        ]
+      },
+      { // Pursuer Mousoleum without paladin's bane - have Portal Pursuer
+        phase: utils.genVarField("stage", "Pursuer Mousoleum"),
+        charm: [
+          { vars: { charm: { "Rift Antiskele": false } } },
+          {
+            fields: { charm: "Rift Antiskele" },
+            opts: { exclude: [ 'Skeletal Champion' ] }
+          }
+        ]
+      },
+      { // Pursuer Mousoleum with paladin's bane - have Portal Pursuer
+        phase: [{
+          vars: { stage: { "Pursuer Mousoleum": true } },
+          fields: { stage: "Pursuer Mousoleum" + paladinBaneSuffix }
+        }],
+        charm: [
+          { vars: { charm: { "Rift Antiskele": false } } },
+          {
+            fields: { charm: "Rift Antiskele" },
+            opts: { exclude: [ 'Skeletal Champion', 'Portal Paladin' ] }
+          }
+        ]
+      },
+      { // Security Chamber - always with paladin's bane
+        phase: [{
+          vars: { stage: { "Security": true } },
+          fields: { stage: "Security" + paladinBaneSuffix }
+        }],
+        charm: [
+          {
+            vars: { charm: { "Rift Antiskele": false } },
+            opts: { exclude: [ 'Portal Pursuer' ] }
+          },
+          {
+            fields: { charm: "Rift Antiskele" },
+            opts: { exclude: [ 'Skeletal Champion', 'Portal Pursuer' ] }
           }
         ]
       }
