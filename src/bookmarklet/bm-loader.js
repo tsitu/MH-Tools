0759bda512035983e8fcc59c3ad64f167ae63a1a
Jack Smith <DevJackSmith@users.noreply.github.com>
2019-03-15 13:56:02 -0600

    Update bm-loader.js
    Adding local storage bypass

--- src/bookmarklet/bm-loader.js
+++ src/bookmarklet/bm-loader.js
@@ -1,23 +1,36 @@
 (function() {
   localStorage.setItem("tsitu-latest-sha", 0);
-
-  getLatestSHA().then(function(response) {
-    if (JSON.parse(response)[0] !== undefined) {
+  var localMenuUrl = localStorage.getItem("tsitu-local-menu-url");
+  
+  if (localMenuUrl) {
+    loadMenu("local");
+  } else {
+    getLatestSHA().then(loadMenu(response));
+  }
+  
+  function loadMenu(response) {
+    if (response === "local") {
+      var cdn = localMenuUrl;
+    }
+    else if (JSON.parse(response)[0] !== undefined) {
       var sha = JSON.parse(response)[0].sha;
       localStorage.setItem("tsitu-latest-sha", sha);
 
       // Load bookmarklet-menu based on SHA
-      var el = document.createElement("script");
       var cdn =
         "https://cdn.jsdelivr.net/gh/tsitu/MH-Tools@" +
         sha +
         "/src/bookmarklet/bm-menu.min.js";
-      el.src = cdn;
-      document.body.appendChild(el);
-      el.onload = function() {
-        el.remove();
-      };
+    } else {
+        console.log("Couldn't find menu url");
+        return;
     }
+    var el = document.createElement("script");
+    el.src = cdn;
+    document.body.appendChild(el);
+    el.onload = function() {
+      el.remove();
+    };
   });
 
   function getLatestSHA() {
