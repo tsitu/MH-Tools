417616b33408184dd0d546d6a6bc0bbc08fdf586
Hank McCord <hymccord@gmail.com>
2023-07-28 19:48:01 -0400

    Update Balack's for 2023-01-17 patch

--- data/pop-js/balacks-cove.js
+++ data/pop-js/balacks-cove.js
@@ -3,13 +3,31 @@ const utils = require("../_utils");
 module.exports = {
   default: {
     location: utils.genVarField("location", "Balack's Cove"),
-    cheese: utils.genVarField("cheese", [
-      "Vanilla Stilton",
-      "Vengeful Vanilla Stilton"
-    ])
+    after: utils.genVarField("after", 1673960400), // 2023-01-17T13:00:00Z
   },
   series: [
     {
+      cheese: utils.genVarField("cheese", "Vanilla Stilton"),
+      phase: utils.genVarField("stage", "Low Tide"),
+      config: [
+        {
+          opts: {
+            include: [
+              "Brimstone",
+              "Davy Jones",
+              "Derr Lich",
+              "Elub Lich",
+              "Enslaved Spirit",
+              "Nerg Lich",
+              "Tidal Fisher",
+              "Twisted Fiend"
+            ]
+          }
+        }
+      ]
+    },
+    {
+      cheese: utils.genVarField("cheese", "Vengeful Vanilla Stilton"),
       phase: utils.genVarField("stage", "Low Tide"),
       config: [
         {
@@ -22,7 +40,6 @@ module.exports = {
               "Elub Lich",
               "Enslaved Spirit",
               "Nerg Lich",
-              "Tidal Fisher",
               "Twisted Fiend"
             ]
           }
@@ -30,20 +47,18 @@ module.exports = {
       ]
     },
     {
+      cheese: utils.genVarField("cheese", "Vanilla Stilton"),
       phase: utils.genVarField("stage", "Medium Tide"),
       config: [
         {
           opts: {
             include: [
-              "Balack the Banished",
               "Brimstone",
               "Davy Jones",
               "Derr Lich",
               "Elub Lich",
               "Enslaved Spirit",
               "Nerg Lich",
-              "Riptide",
-              "Tidal Fisher",
               "Twisted Fiend"
             ]
           }
@@ -51,6 +66,26 @@ module.exports = {
       ]
     },
     {
+      cheese: utils.genVarField("cheese", "Vengeful Vanilla Stilton"),
+      phase: utils.genVarField("stage", "Medium Tide"),
+      config: [
+        {
+          opts: {
+            include: [
+              "Balack the Banished",
+              "Derr Lich",
+              "Elub Lich",
+              "Nerg Lich",
+            ]
+          }
+        }
+      ]
+    },
+    {
+      cheese: utils.genVarField("cheese", [
+        "Vanilla Stilton",
+        "Vengeful Vanilla Stilton",
+      ]),
       phase: utils.genVarField("stage", "High Tide"),
       config: [
         {
@@ -60,5 +95,15 @@ module.exports = {
         }
       ]
     }
-  ]
+  ],
+  /**
+   *
+   * @param {{stage: string, location: string, cheese: string, mouse: string, attraction: string, sample: number}[]} data
+   * @returns {{stage: string, location: string, cheese: string, mouse: string, attraction: string, sample: number}[]}
+   */
+    postProcess: function(data) {
+      data.push({stage: "High Tide", location: "Balack's Cove", cheese: "Vanilla Stilton", mouse: "Riptide", attraction: "100.00%", sample: 1});
+      data.push({stage: "High Tide", location: "Balack's Cove", cheese: "Vengeful Vanilla Stilton", mouse: "Riptide", attraction: "100.00%", sample: 1});
+      return data;
+    },
 };
