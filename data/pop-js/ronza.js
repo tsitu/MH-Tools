e2fc841e5ef9cff5c2d6334607c3d316c2d1f334
Hank McCord <hymccord@gmail.com>
2024-07-26 04:51:02 -0400

    Ronza 2024 Updates (#305)
    * Updated mouse-power-effs
    * Added new traps and mice
    * RTS population AR update
    * Updated Chrome charms
    
    - https://www.mousehuntgame.com/newspost.php?news_post_id=863
    
    ---------
    
    Co-authored-by: Tran Situ <trancsitu@gmail.com>

--- data/pop-js/ronza.js
+++ data/pop-js/ronza.js
@@ -1,6 +1,6 @@
 const utils = require("../_utils");
 
-const cheeses = ["SB+", "Gouda", "Brie", "Swiss", "Marble", "Rockforth"];
+const cheeses = ["SB+", "Gouda", "Brie", "Swiss", "Marble"];
 
 module.exports = {
   default: {
@@ -14,6 +14,42 @@ module.exports = {
         {
           opts: {
             exclude: ["White", "Brown", "Grey"]
+          }
+        }
+      ]
+    },
+    {
+      cheese: utils.genVarField("cheese", "Rockforth"),
+      after: utils.genVarField("after", 1721833200), // 2024-070-24T15:00:00Z
+      config: [
+        {
+          opts: {
+            include: [
+              "Elven Princess",
+              "Goldleaf",
+              "Lucky",
+              "Longtail",
+              "Moussile",
+              "Pugilist",
+              "Rockstar",
+            ]
+          }
+        }
+      ]
+    },
+    {
+      cheese: utils.genVarField("cheese", "Abominable Asiago"),
+      after: utils.genVarField("after", 1721833200), // 2024-070-24T15:00:00Z
+      config: [
+        {
+          opts: {
+            include: [
+              "Living Ice",
+              "Mammoth",
+              "Rimeus Polarblast",
+              "Snow Bowler",
+              "Yeti",
+            ]
           }
         }
       ]
