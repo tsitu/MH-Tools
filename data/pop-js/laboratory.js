2f8e6f4adece17fc44a217cce9f91474890af7f0
Hank McCord <hymccord@gmail.com>
2026-06-26 13:36:27 -0400

    Fix Lab due to permanent Glitchpaw addition

--- data/pop-js/laboratory.js
+++ data/pop-js/laboratory.js
@@ -3,12 +3,10 @@ const utils = require("../_utils");
 module.exports = {
   default: {
     location: utils.genVarField("location", "Laboratory"),
-    after: utils.genVarField("after", 1539790440)
+    // 2024-03-20: Glitchpaw added as permanent mouse
+    after: utils.genVarField("after", 1710907200)
   },
   series: [
-    {
-      cheese: utils.genVarField("cheese", ["SB+", "Brie"])
-    },
     {
       cheese: utils.genVarField("cheese", [
         "Gouda",
@@ -16,21 +14,59 @@ module.exports = {
         "Marble",
         "Cheddar"
       ]),
-      config: [
-        {
-          opts: {
-            exclude: ["Burglar"]
-          }
-        }
-      ]
+      mice: utils.genInclude([
+        "Bionic",
+        "Clumsy Chemist",
+        "Glitchpaw",
+        "Sludge Scientist",
+        "Squeaker Bot",
+      ])
+    },
+    {
+      cheese: utils.genVarField("cheese", "Brie"),
+      mice: utils.genInclude([
+        "Bionic",
+        "Burglar",
+        "Clumsy Chemist",
+        "Glitchpaw",
+        "Sludge Scientist",
+        "Squeaker Bot"
+      ])
+    },
+    {
+      cheese: utils.genVarField("cheese", "SB+"),
+      mice: utils.genInclude([
+        "Bionic",
+        "Black Widow",
+        "Burglar",
+        "Clumsy Chemist",
+        "Glitchpaw",
+        "Sludge Scientist",
+        "Squeaker Bot"
+      ])
     },
     {
       cheese: utils.genVarField("cheese", [
         "Radioactive Blue",
         "Rancid Radioactive Blue",
         "Magical Rancid Radioactive Blue",
-        "Limelight"
+      ]),
+      mice: utils.genInclude([
+        "Black Widow",
+        "Monster",
+        "Mutated Brown",
+        "Mutated Grey",
+        "Mutated White",
       ])
     }
-  ]
+  ],
+    /**
+   *
+   * @param {import('../_utils').AttractionData[]} data
+   * @returns {import('../_utils').AttractionData[]}
+   */
+  postProcess: function(data) {
+    data.push({location: "Laboratory", cheese: "Limelight", mouse: "Mutated Mole", attraction: "100.00%", sample: 1});
+    return data;
+  },
 };
